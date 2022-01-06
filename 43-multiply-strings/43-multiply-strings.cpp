e11f7d602f917cae7f5d9774657e2553aeac27ee
Umesh Saud <61150808+umsisamess@users.noreply.github.com>
2022-01-06 18:45:14 +0545

    Time: 4 ms (90.54%), Space: 6.7 MB (62.37%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -1,81 +1,30 @@
 class Solution {
 public:
-    string product(int a, string nums,int zeros){
-        string s = "";
-        int prod,carry = 0;
-        for(int j = nums.length()-1;j>=0;j--){
-            int b = nums[j]-'0';
-            prod = a*b;
-            prod += carry;
-            carry = prod/10;
-            int rem = prod % 10;
-            char r = '0'+rem;
-            s = r+s;
-        }
-        if(carry>0){
-            char c = '0'+carry;
-            s = c+s;
-        }
-        for(int j=0;j<zeros;j++){
-            s = s + '0';
-        }
-        return s;
-    }
-    
-    string sum(string s1, string s2){
-        if(s1==""){
-            return s2;
-        }
-        int l1 = s1.length();
-        int l2 = s2.length();
-        if(l2>l1){
-            for(int i=0;i<l2-l1;i++){
-                s1 = "0"+s1;
-            }
-            l1 = l2;
-        }
-        else{
-            for(int i=0;i<l1-l2;i++){
-                s2 = "0"+s2;
-            }
-            l2 = l1;
-        }
-        
-        int sum,carry = 0;
-        string s = "";
-        for(int i=l1-1;i>=0;i--){
-            int a = s1[i]-'0';
-            int b = s2[i]-'0';
-            sum = a + b + carry;
-            carry = sum/10;
-            int rem = sum % 10;
-            char r = '0'+rem;
-            s = r+s;
-        }
-        if(carry>0){
-            char c = '0'+carry;
-            s = c+s;
-        }
-        
-        return s;
-    }
-    
     string multiply(string num1, string num2) {
         if(num1=="0" || num2=="0"){
             return "0";
         }
-        int l1 = num1.length();
-        int a,b;
-        int zeros = 0;
-        string s1="",s2="";
+        int l1 = num1.size();
+        int l2 = num2.size();
+        vector<int> sum(l1+l2,0);
+
         for(int i=l1-1;i>=0;i--){
-            a = num1[i]-'0';
-            cout<<a<<" ";
-            s2 = product(a,num2,zeros);
-            s1 = sum(s1,s2);
-            zeros++;
+            for(int j=l2-1;j>=0;j--){
+                sum[i+j+1] += (num1[i]-'0')*(num2[j]-'0');
+                sum[i+j] += sum[i+j+1]/10;
+                sum[i+j+1] %= 10;
+            }
         }
         
-        return s1;
+        string ans = "";
+        int i = 0;
+        while(sum[i]==0){
+            i++;
+        }
+        while(i<l1+l2){
+            ans += ('0'+sum[i]);
+            i++;
+        }
+        return ans;
     }
 };
