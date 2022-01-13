823dec51b79ba8ff39c1f10ee08d99312e7bacb2
Umesh Saud <61150808+umsisamess@users.noreply.github.com>
2022-01-13 15:04:14 +0545

    Time: 0 ms (100.00%), Space: 5.9 MB (33.40%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     double angleClock(int hour, int minutes) {
         hour %= 12;
-        double ans;
-        ans = 30*hour-1.0*11/2*minutes;
-        
+        double ans = 30*hour-1.0*11/2*minutes;
         return min(abs(ans),360-abs(ans));
     }
 };
