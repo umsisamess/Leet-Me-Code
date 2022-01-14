1c67bb4ea736b05b36ac4921b0b1f64de6db3b12
Umesh Saud <61150808+umsisamess@users.noreply.github.com>
2022-01-14 19:44:23 +0545

    Time: 6 ms (20.93%), Space: 7 MB (52.53%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -2,34 +2,25 @@ class Solution {
 public:
     int myAtoi(string s) {
         bool sign = true;
-        long ans = 0;
-        bool characterRead = false;
-        for(int i=0;i<s.length();i++){
-            if(s[i]>='0' && s[i]<='9'){
-                ans = ans*10+(s[i]-'0');
-                if(sign==false && ans>=pow(2,31)){
-                    return INT_MIN;
-                }
-                else if(sign==true && ans>INT_MAX){
-                    return INT_MAX;
-                }
+        int ans = 0,i=0,n = s.length();
+        
+        while(s[i]==' ' && i<n) i++;
+        
+        if(s[i]=='+'){
+            sign = true;
+            i++;
+        }
+        else if(s[i]=='-'){
+            sign = false;
+            i++;
+        }
+        
+        while(s[i]>='0' && s[i]<='9'){
+            if(ans>INT_MAX/10 || (ans==INT_MAX/10 && s[i]>'7')){
+                return sign?INT_MAX:INT_MIN;
             }
-            else if(s[i]==' '){
-                if(characterRead) break;
-            }
-            else if(s[i]=='+'){
-                if(characterRead) break;
-                sign = true;
-            }
-            else if(s[i]=='-'){
-                if(characterRead) break;
-                sign = false;
-            }
-            else{
-                break;
-            }
-            
-            if(s[i]!=' ') characterRead = true;
+            ans = ans*10+(s[i]-'0');
+            i++;
         }
         return sign?ans:-ans;
     }
