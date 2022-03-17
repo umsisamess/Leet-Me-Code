988cfe2d85f430b2194f90771f5d03f2fa094d8c
Umesh Saud <61150808+umsisamess@users.noreply.github.com>
2022-03-17 17:18:44 +0545

    Time: 0 ms (100.00%), Space: 6.2 MB (45.05%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -2,41 +2,17 @@ class Solution {
 public:
     int scoreOfParentheses(string s) {
         stack<int> st;
-        int n = s.length();
-        int level = 0;
-        int prevpoppedlevel = -1;
-        vector<int> prevpoppedans(n+1,0);
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            if(s[i]=='('){
-                st.push(++level);
+        int val = 0;
+        for(auto i:s){
+            if(i=='('){
+                st.push(val);
+                val = 0;
             }
             else{
-                if(prevpoppedlevel==-1){
-                    ans += 1;
-                    prevpoppedans[level] = ans;
-                }
-                else{
-                    cout<<prevpoppedlevel<<" "<<level<<"\n";
-                    if(prevpoppedlevel==level){
-                        ans += 1;
-                        prevpoppedans[level] += 1;
-                    }
-                    else if(prevpoppedlevel>level){
-                        ans += prevpoppedans[prevpoppedlevel];
-                        prevpoppedans[level] += 2*prevpoppedans[prevpoppedlevel];
-                        prevpoppedans[prevpoppedlevel] = 0;
-                    }
-                    else{
-                        ans += 1;
-                        prevpoppedans[level] = 1;
-                    }
-                }
-                prevpoppedlevel = level;
-                --level;
+                val = st.top()+max(2*val,1);
+                st.pop();
             }
         }
-        cout<<"\n";
-        return ans;
+        return val;
     }
 };
