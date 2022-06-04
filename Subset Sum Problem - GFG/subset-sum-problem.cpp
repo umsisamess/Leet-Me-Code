f449e265e81c9261456bf81d91eae58cdcb2d982
Umesh Saud <61150808+umsisamess@users.noreply.github.com>
2022-06-05 03:55:08 +0530

    Added solution - LeetHub

---
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    bool isSubsetSum(vector<int>arr, int sum){
+        vector<bool> dp(sum+1);
+        dp[0] = true;
+        
+        for(auto i : arr){
+            for(int j=sum;j>=i;j--){
+                if(dp[j-i]==true){
+                    dp[j] = true;
+                }
+            }
+        }
+        
+        return dp[sum];
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, sum;
+        cin >> N;
+        vector<int> arr(N);
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+        cin >> sum;
+        
+        Solution ob;
+        cout << ob.isSubsetSum(arr, sum) << endl;
+    }
+    return 0; 
+} 
+  // } Driver Code Ends
